84d7f3325eee8c6e8c1fb227dc9866346181e6a8
Raaja Selvanaathan Datchanamourthy <raaja-selvanaathan.datchanamourthy@edu.dsti.institute>
2026-04-09 17:16:46 +0200

    Model training pipeline done

--- src/training/Trainer.py
+++ src/training/Trainer.py
@@ -2,6 +2,9 @@ import pandas as pd
 import yaml
 from pathlib import Path
 from src.training.LogisticRegressionTrainer import LogisticRegressionTrainer
+from src.training.RandomForestTrainer import RandomForestTrainer
+from src.training.XGBoostTrainer import XGBoostTrainer
+from src.training.CatBoostTrainer import CatBoostTrainer
 from src.preprocessing.data_cleaning import drop_outliers
 from src.utils.logger import logger
 
@@ -22,7 +25,22 @@ def train_logistic_regression(df:pd.DataFrame,labels:pd.Series):
     log_trainer.train()
     log_trainer.predict()
 
+def train_rf(df:pd.DataFrame,labels:pd.Series):
+    rf_trainer = RandomForestTrainer(df,labels)
+    rf_trainer.train()
+    rf_trainer.predict()
+
+def train_xgb(df:pd.DataFrame,labels:pd.Series):
+    xgb_trainer = XGBoostTrainer(df,labels)
+    xgb_trainer.train()
+    xgb_trainer.predict()
+
+def train_rf(df:pd.DataFrame,labels:pd.Series):
+    cat_trainer = CatBoostTrainer(df,labels)
+    cat_trainer.train()
+    cat_trainer.predict()
+
 
 if __name__ in '__main__':
     df,labels = get_dataset()
-    train_logistic_regression(df,labels)
+    train_xgb(df,labels)
